f61195f933831cd5bbd30f5b9e537ea09ab12b75
robertocarlosmedina <robertocarlosmedina.dev@gmail.com>
2022-04-02 18:34:10 -0100

    Implementing python script to remove duplicate sentence from a file
    Also remove the duplicated phrases from the cv.txt

---
+++ remove_duplicated.py
@@ -0,0 +1,20 @@
+file_reader = open("cv.txt", "r")
+file_writer = open("cv-fixed.txt", "a")
+
+phrases = []
+all_lines = file_reader.readlines()
+
+print(f"Total lines with duplicates: {len(all_lines)}")
+for line in all_lines:
+    line = line[0:-1]
+    if line not in phrases:
+        phrases.append(line)
+    # else:
+    #     print(line[0:-1])
+
+print(f"Toltal lines without duplicates: {len(phrases)}")
+for phrase in phrases:
+    file_writer.write(f"{phrase}\n")
+
+file_writer.close()
+file_reader.close()
