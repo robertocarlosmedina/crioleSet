001caaac2b5629bdcf5f172c7c6539bd991f9074
Roberto <robertocarlosmedina.dev@gmail.com>
2022-05-11 23:26:56 -0100

    Updates

--- get_csv_data.py
+++ get_csv_data.py
@@ -4,21 +4,27 @@ import pandas
 data = pandas.read_csv('CVEN-Dataset.csv')
 cv_file_writer = open("train.cv", "w")
 pt_file_writer = open("train.pt", "w")
+en_file_writer = open("train.en", "w")
 
 pt_translations = []
 cv_translations = []
+en_translations = []
 
 [pt_translations.append(row["Portuguese"]) for _ , row in data.iterrows()]
 [cv_translations.append(row["Capverdian Creole"]) for _ , row in data.iterrows()]
+[en_translations.append(row["English"]) for _ , row in data.iterrows()]
 
 index = 1
-for pt, cv in zip(pt_translations, cv_translations):
+print(cv_translations)
+for pt, cv, en in zip(pt_translations, cv_translations, en_translations):
     print(f"{index}: {cv}: {pt}")
     cv_file_writer.write(f"{cv}\n")
     pt_file_writer.write(f"{pt}\n")
+    en_file_writer.write(f"{en}\n")
     index += 1
 
 cv_file_writer.close()
 pt_file_writer.close()
+en_file_writer.close()
 # print(pt_translations[0])
 # print(cv_translations[0])
